5b7b978ad1c950f9ad450a29edac428cfe37a6be
Al Zee <z@alz.ee>
2022-04-17 13:11:15 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    let mut i = String::new();
-    io::stdin()
-        .read_line(&mut i)
-        .expect("fuck");
+    let s = rand::thread_rng().gen_range(1..101);
+    println!("{}", s);
 
-    println!("{}", i);
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("fuck");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("number you stupid");
+                continue;
+            }
+        };
+
+        println!("{} {}", guess, s);
+
+        match guess.cmp(&s) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("yes");
+                break;
+            }
+        }
+    }
 }
